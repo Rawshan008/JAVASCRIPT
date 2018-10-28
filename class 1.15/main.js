0c4390255f71871454f264591079445511186273
Rawshan008 <pust.love008@gmail.com>
2018-10-29 00:19:24 +0600

    Class 1.5 Loop, while and do while

---
+++ class 1.15/main.js
@@ -0,0 +1,16 @@
+
+
+var a = 0;
+// while loop, but causion do not apply infinity loop;
+// while(a<5){
+//     console.log(a);
+//     a++;
+//     console.log("Loop");
+// }
+
+do{
+    console.log(a);
+    a++;
+    console.log("Do While Loop.");
+}while(a<5); 
+ // same as while loop, but it has problem. ati akber excute kore, then check kore.
