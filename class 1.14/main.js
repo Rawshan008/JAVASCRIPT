1846c9b9c89a7a4cd50ed94873770dde8df954fb
Rawshan008 <pust.love008@gmail.com>
2018-10-28 23:40:44 +0600

    Class 1.14 Short Circuit Evulation for if statement

---
+++ class 1.14/main.js
@@ -0,0 +1,11 @@
+
+
+var a = 10;
+if(a == 10){
+    console.log("Statement is true"); // excutable
+}
+// if a is true then output is excute
+if(a){
+    console.log("Statement is true"); // same as upper
+}
+a && console.log("Statement is true"); // same as upper two
