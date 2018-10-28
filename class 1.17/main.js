807f147194534d9d41adf3dfddac6c5e8f046095
Rawshan008 <pust.love008@gmail.com>
2018-10-29 00:47:07 +0600

    For Loop, print 1-50, with odd and even number

---
+++ class 1.17/main.js
@@ -0,0 +1,14 @@
+
+
+// 1 to 50 print
+for(var a = 1; a<=50; a++){
+    console.log(a);
+}
+// 1 to 50 odd number print
+for(var a = 1; a<=50; a+=2){
+    console.log(a);
+}
+
+for(var a = 0; a <=50; a+=2){
+    console.log(a);
+}
