1e2ccdb38fd66c432f20e57bfcff9c612d503489
Rawshan008 <pust.love008@gmail.com>
2018-10-28 17:50:52 +0600

    class 12, ternary operator, project odd even

---
+++ class 1.12/main.js
@@ -0,0 +1,9 @@
+var a = 60;
+if(a%2==0){
+    console.log("Number is even");
+}else{
+    console.log("number is odd");
+}
+// to easy solution of above code 
+
+a%2==0?console.log("Number is even"): console.log("Number is Odd");
