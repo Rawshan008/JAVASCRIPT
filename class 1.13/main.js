597c6bf049466987c9e10d74683dddc91b58b129
Rawshan008 <pust.love008@gmail.com>
2018-10-28 18:37:22 +0600

    Class 1.13, complex login with male and Female biar boyos

---
+++ class 1.13/main.js
@@ -0,0 +1,9 @@
+
+var age = 18;
+var gender = "Famale";
+
+if((age > 20 && gender == "Male") || (age >17 && gender == "Famale")){
+    console.log("Biar boyos hoise");
+}else{
+    console.log("Biar boyos hoi nai");
+}
