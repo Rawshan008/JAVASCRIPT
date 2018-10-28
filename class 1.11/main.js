1fdc06ce16c927fef6824d7f017a7b487b280ab5
Rawshan008 <pust.love008@gmail.com>
2018-10-28 17:36:11 +0600

    Class 1.11, Grading System by if-else

---
+++ class 1.11/main.js
@@ -0,0 +1,17 @@
+var number = 12;
+if(number >= 80){
+    console.log("A+");
+}else if(number >= 70){
+    console.log("A");
+}else if(number >= 60){
+    console.log("A-");
+}else if(number >= 50){
+    console.log("B");
+}else if(number >= 40){
+    console.log("C");
+}else if(number >= 33){
+    console.log("D");
+}
+else{
+    console.log("Fail");
+}
